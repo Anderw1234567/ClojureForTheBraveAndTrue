eaa80d364745ed652eccba12355bfda247e91012
Andrew Smith <adsmitty70@yahoo.com>
2023-12-17 17:15:55 -0500

    initial commit

---
+++ projects/clojure-noob/src/clojure_noob/fornoobs.clj
@@ -0,0 +1,23 @@
+(ns src.clojure-noob.fornoobs)
+
+(if true
+  "zeus"
+  "aquaman")
+
+(if ""
+ (do (println "succces")
+     "zeus")
+  (do (println "fail")
+      "aqueman"))
+
+(nil? 1)
+
+(nil? nil)
+(nil? false)
+
+
+(if "bears"
+  "no bears"
+  "or bears")
+ 
+(= 1 2)
